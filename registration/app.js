bfeab25667d938696fa52a8fd1ed1072fc12e045
Jason E. Robinson <jason.robinson@auspex-labs.com>
2024-03-05 00:55:06 -0500

    Apply Formatting

--- registration/app.js
+++ registration/app.js
@@ -1,87 +1,87 @@
-import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    registerUser();
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  registerUser();
 });
 
-document.getElementById('passwordResetRequestForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    requestPasswordReset();
+document.getElementById("passwordResetRequestForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  requestPasswordReset();
 });
 
-document.getElementById('passwordResetForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    resetPassword();
+document.getElementById("passwordResetForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  resetPassword();
 });
 
 var poolData = {
-    UserPoolId: 'YOUR_COGNITO_USER_POOL_ID',
-    ClientId: 'YOUR_COGNITO_CLIENT_ID'
+  UserPoolId: "YOUR_COGNITO_USER_POOL_ID",
+  ClientId: "YOUR_COGNITO_CLIENT_ID",
 };
 
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 function registerUser() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-    var attributeList = [];
+  var attributeList = [];
 
-    var dataEmail = {
-        Name: 'email',
-        Value: email
-    };
+  var dataEmail = {
+    Name: "email",
+    Value: email,
+  };
 
-    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-    attributeList.push(attributeEmail);
+  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+  attributeList.push(attributeEmail);
 
-    userPool.signUp(email, password, attributeList, null, function(err, result) {
-        if (err) {
-            alert(err.message || JSON.stringify(err));
-            return;
-        }
-        var cognitoUser = result.user;
-        console.log('User registration successful: ' + cognitoUser.getUsername());
-    });
+  userPool.signUp(email, password, attributeList, null, function (err, result) {
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+      return;
+    }
+    var cognitoUser = result.user;
+    console.log("User registration successful: " + cognitoUser.getUsername());
+  });
 }
 
 function requestPasswordReset() {
-    var email = document.getElementById('resetEmail').value;
-    var userData = {
-        Username: email,
-        Pool: userPool
-    };
+  var email = document.getElementById("resetEmail").value;
+  var userData = {
+    Username: email,
+    Pool: userPool,
+  };
 
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.forgotPassword({
-        onSuccess: function () {
-            console.log('Password reset request successful');
-            document.getElementById('passwordResetForm').style.display = 'block';
-        },
-        onFailure: function(err) {
-            alert(err.message || JSON.stringify(err));
-        }
-    });
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  cognitoUser.forgotPassword({
+    onSuccess: function () {
+      console.log("Password reset request successful");
+      document.getElementById("passwordResetForm").style.display = "block";
+    },
+    onFailure: function (err) {
+      alert(err.message || JSON.stringify(err));
+    },
+  });
 }
 
 function resetPassword() {
-    var email = document.getElementById('resetEmail').value;
-    var verificationCode = document.getElementById('verificationCode').value;
-    var newPassword = document.getElementById('newPassword').value;
+  var email = document.getElementById("resetEmail").value;
+  var verificationCode = document.getElementById("verificationCode").value;
+  var newPassword = document.getElementById("newPassword").value;
 
-    var userData = {
-        Username: email,
-        Pool: userPool
-    };
+  var userData = {
+    Username: email,
+    Pool: userPool,
+  };
 
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.confirmPassword(verificationCode, newPassword, {
-        onSuccess() {
-            console.log('Password reset successful');
-        },
-        onFailure(err) {
-            alert(err.message || JSON.stringify(err));
-        }
-    });
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  cognitoUser.confirmPassword(verificationCode, newPassword, {
+    onSuccess() {
+      console.log("Password reset successful");
+    },
+    onFailure(err) {
+      alert(err.message || JSON.stringify(err));
+    },
+  });
 }
